b22668c90f8540c6aee88333b9c2e824c2eb3199
Max Maischein <corion@corion.net>
2017-03-06 22:10:29 +0100

    Add some documentation

--- lib/Net/Fritz/Phonebook.pm
+++ lib/Net/Fritz/Phonebook.pm
@@ -14,13 +14,29 @@ use Data::Dumper;
 
 =head1 NAME
 
-Net::Fritz::PhoneBook - update the Fritz!Box phonebook from Perl
+Net::Fritz::Phonebook - manage the Fritz!Box phonebook from Perl
 
 =head1 SYNOPSIS
+
+  my $fb = Net::Fritz::Box->new(
+      username => $username,
+      password => $password,
+      upnp_url => $host,
+  );
+  my $device = $fb->discover;
+  if( my $error = $device->error ) {
+      die $error
+  };
+  my $services = $device->find_service_names(qr/X_AVM-DE_OnTel/);
+  my $service = $services->data->[0];
+
+  my @phonebooks = Net::Fritz::Phonebook->list($service);
 
 This module uses the API exposed by the Fritz!Box via TR064 to read, create and
 update contacts in a phone book. This uses the C<X_AVM-DE_OnTel> service, which
 is specific to the AVM Fritz!Box line of products.
+
+=head1 ACCESSORS
 
 =cut
 
@@ -35,6 +51,14 @@ has '_xs' => (
     }
 );
 
+=head2 C<< id >>
+
+  print $phonebook->id;
+
+The ID of the phone book on the Fritz!Box
+
+=cut
+
 has 'id' => (
     is => 'ro',
 );
@@ -47,12 +71,30 @@ has 'metadata' => (
     },
 );
 
+=head2 C<< name >>
+
+  print $phonebook->name;
+
+The user visible name of the phone book on the Fritz!Box
+
+=cut
+
 has 'name' => (
     is => 'lazy',
     default => sub($self) {
         $self->metadata->{NewPhonebookName}
     },
 );
+
+=head2 C<< url >>
+
+  print $phonebook->url;
+
+The URL of the phone book on the Fritz!Box
+
+This URL is used to access the phone book contents.
+
+=cut
 
 has 'url' => (
     is => 'lazy',
@@ -69,14 +111,45 @@ has 'content' => (
     },
 );
 
+=head2 C<< entries >>
+
+  for my $entry ( @{ $phonebook->entries }) {
+      print $entry->name
+  };
+
+Arrayref of the entries in this phone book. Each entry is
+a L<Net::Fritz::Phonebook::Entry>.
+
+=cut
+
 has 'entries' => (
     is => 'lazy',
     builder => 1,
 );
 
+=head1 METHODS
+
+=head2 C<< $phonebook->create >>
+
+  $phonebook->create();
+
+Creates the phone book on the FritzBox. Entries of this phone book are not
+saved.
+
+=cut
+
 sub create( $self, %options ) {
     $self->service->call('AddPhonebook')->data
 }
+
+=head2 C<< $phonebook->delete >>
+
+  $phonebook->delete();
+
+Deletes the phone book on the FritzBox. All entries of this phone book are also
+deleted with the phone book.
+
+=cut
 
 sub delete( $self, %options ) {
     $self->service->call('DeletePhonebook', NewPhonebookID => $self->id )->data
@@ -86,6 +159,14 @@ sub _build_entries( $self, %options ) {
     my $c = $self->content;
     [map { Net::Fritz::PhonebookEntry->new( phonebook => $self, contact => [$_] ) } @{ $self->content->{phonebook}->[0]->{contact} }];
 }
+
+=head2 C<< $phonebook->add_entry >>
+
+  $phonebook->add_entry( $new_contact );
+
+Saves an entry in the phone book on the FritzBox.
+
+=cut
 
 sub add_entry( $self, $entry ) {
     my $s = $entry->build_structure;
@@ -98,6 +179,21 @@ sub add_entry( $self, $entry ) {
         NewPhonebookEntryData => $xml,
     );
 };
+
+=head2 C<< Net::Fritz::Phonebook->list( $service ) >>
+
+  my $device = $fb->discover;
+  my $services = $device->find_service_names(qr/X_AVM-DE_OnTel/);
+  my @phonebooks = Net::Fritz::Phonebook->list( $services->data->[0] );
+
+=cut
+
+sub list( $class, $service ) {
+    my $d = $service->call('GetPhonebookList')->data;
+    return
+      map { $class->new( id => $_, service => $service ) }
+        split /,/, $service->call('GetPhonebookList')->data->{NewPhonebookList}
+}
 
 package Net::Fritz::PhonebookEntry;
 use strict;
@@ -277,7 +373,7 @@ has 'prio' => (
   fax_work
 
 All other strings get displayed as "Sonstige" but get preserved.
-  
+
 =cut
 
 has 'type' => (
