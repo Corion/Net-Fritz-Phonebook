95fddd18d6070b31a4f13e958cdda5e215580c4b
Max Maischein <corion@corion.net>
2020-11-24 20:25:09 +0100

    Update copyright year

--- lib/Net/Fritz/Phonebook.pm
+++ lib/Net/Fritz/Phonebook.pm
@@ -324,7 +324,7 @@ Max Maischein C<corion@cpan.org>
 
 =head1 COPYRIGHT (c)
 
-Copyright 2017 by Max Maischein C<corion@cpan.org>.
+Copyright 2017-2020 by Max Maischein C<corion@cpan.org>.
 
 =head1 LICENSE
 
